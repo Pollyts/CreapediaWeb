063c27fa097642f76dbb55b2977da013c5288e48
tspo <polia.tselisheva@mail.ru>
2021-05-05 23:38:32 +0500

    Настроила стили для панели инструментов

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import addimg from './AddItem.png';
+import addfolder from './AddFolder.png';
+import deleteimg from './Delete.png';
+import editimg from './Edit.png';
+import exportimg from './Export.png';
+import importimg from './Import.png';
+import searchimg from './Search.png';
+import settingsimg from './Settings.png';
 import './Toolbar.css';
 
 export default function Toolbar({typeof_parentel, parentid, userid}) {
@@ -10,10 +17,13 @@ export default function Toolbar({typeof_parentel, parentid, userid}) {
             <div className="LayoutCenter">
             <div className="Toolbar">
             <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
+            <button><img src={addfolder} /></button>
+            <button><img src={deleteimg} /></button>
+            <button><img src={editimg} /></button>
+            <button><img src={exportimg} /></button>
+            <button><img src={importimg} /></button>
+            <button><img src={searchimg} /></button>
+            <button><img src={settingsimg} /></button>
             </div>
             </div>
           )
@@ -24,10 +34,13 @@ export default function Toolbar({typeof_parentel, parentid, userid}) {
             <div className="LayoutCenter">
             <div className="Toolbar">
             <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
+            <button><img src={addfolder} /></button>
+            <button><img src={deleteimg} /></button>
+            <button><img src={editimg} /></button>
+            <button><img src={exportimg} /></button>
+            <button><img src={importimg} /></button>
+            <button><img src={searchimg} /></button>
+            <button><img src={settingsimg} /></button>
             </div>
             </div>
           )
@@ -38,10 +51,13 @@ export default function Toolbar({typeof_parentel, parentid, userid}) {
             <div className="LayoutCenter">
             <div className="Toolbar">
             <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
-            <button><img src={addimg} /></button>
+            <button><img src={addfolder} /></button>
+            <button><img src={deleteimg} /></button>
+            <button><img src={editimg} /></button>
+            <button><img src={exportimg} /></button>
+            <button><img src={importimg} /></button>
+            <button><img src={searchimg} /></button>
+            <button><img src={settingsimg} /></button>
             </div>
             </div>
           )
